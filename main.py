e5f5f104aae20ba6b4ac71ac8fd56820dd45ef8e
yvan.kombou <yvan.kombou@kaeyros-analytics.com>
2022-05-01 19:22:10 +0200

    =main

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import Tk, Label
+
+
+def print_hi():
+    root = Tk()
+    label = Label(root, text="Galery")
+    label.pack()
+    root.mainloop()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
