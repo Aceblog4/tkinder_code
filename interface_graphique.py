fb6ecfdbb2e74673ec41bae2bd072015f4f2722f
Simon Machielsen <masi1407@student.iepscf-uccle.be>
2022-05-04 15:01:54 +0200

    ajout du logo et tests tkinter

---
+++ interface_graphique.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+#Création de la fenêtre
+
+window = Tk()
+
+#Informations de l'interface
+
+window.title('Projet DevOps')
+window.geometry('960x540')
+window.minsize(600,600)
+window.maxsize(1920,1080)
+window.iconbitmap("logo.ico")
+
+#Affichage de la fenêtre
+
+window.mainloop()
